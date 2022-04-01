1078a4cf5bfd6ae06975a93b12bcede191cf2a9a
Dr. Max Völkel <mv@maxvoelkel.de>
2022-04-01 08:37:23 +0200

    add batch files for java version switch

---
+++ usejava17.bat
@@ -0,0 +1,5 @@
+SET JAVA_HOME=C:\apps\jdk-17
+SET PATH=%JAVA_HOME%\bin;%PATH%
+call java -version
+
+
---
+++ usejava8.bat
@@ -0,0 +1,3 @@
+SET JAVA_HOME=C:\apps\jdk1.8.0_211
+SET PATH=%JAVA_HOME%\bin;%PATH%
+call java -version
