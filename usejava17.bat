2db4c58f8079dffe219f12e8dd837ce9fd345855
Dr. Max Völkel <mv@maxvoelkel.de>
2022-04-07 06:33:55 +0200

    remove batch files

--- usejava17.bat
+++
@@ -1,5 +0,0 @@
-SET JAVA_HOME=C:\apps\jdk-17
-SET PATH=%JAVA_HOME%\bin;%PATH%
-call java -version
-
-
--- usejava8.bat
+++
@@ -1,3 +0,0 @@
-SET JAVA_HOME=C:\apps\jdk1.8.0_211
-SET PATH=%JAVA_HOME%\bin;%PATH%
-call java -version
